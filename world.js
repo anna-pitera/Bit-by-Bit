b1b68a6f6028c9aa38ec423f72f1bead5bb746c6
Anna Pitera <annapitera101@gmail.com>
2024-02-09 17:46:27 -0500

    Added phaser.min.js and tested using Phaser with world.js file

---
+++ world.js
@@ -0,0 +1,25 @@
+let gameState = {}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 450,
+    height: 600,
+    backgroundColor: "#5f2a55",
+    scene: {
+        create
+    }
+}
+
+function preload () {
+
+}
+
+function create () {
+
+}
+
+function update () {
+
+}
+
+const game = new Phaser.Game(config);
