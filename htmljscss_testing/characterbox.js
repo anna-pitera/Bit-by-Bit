10a38ebfc1cd20f63bdb2bd67a2f988fa931acb8
Anna Pitera <annapitera101@gmail.com>
2024-04-24 09:59:46 -0400

    Deleted htmlcssjs_testing in favor of vscodemysite which is now being used for testing Django and frontend stuff without being connected to a real database. In terms of frontend, I added the navbar with centered links to the various parts of the page (Shop, Tasks, and Habits). Also decided to use NES.css, which was something I found early on in the semester that I think will help with the pixel art aesthetic. Working on Django forms and input stuff now.

--- htmljscss_testing/characterbox.js
+++
@@ -1,5 +0,0 @@
-const demoCanvas = document.getElementById("canvas-demo").getContext("2d");
-window.onload = function() {
-    demoCanvas.beginPath();
-    demoCanvas.closePath();
-}
--- mysite/bitbybit/models.py
+++ mysite/bitbybit/models.py
@@ -5,8 +5,6 @@ class Task(models.Model):
         app_label = 'bitbybit'
     
     text = models.CharField(max_length=200)
-    is_done = models.BooleanField(default=False)
-    created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
