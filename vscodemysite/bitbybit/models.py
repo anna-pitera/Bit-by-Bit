f320f3e49f922b4e57ef80adc7db44f48411d4d8
Anna Pitera <annapitera101@gmail.com>
2024-05-06 10:11:49 -0400

    Added working submit button for inputting tasks into the database. Also added a hover effect to the button. Working on listing the tasks in a readable way (I have them in a list, but I'd like for them to look better and not just in a table).

--- vscodemysite/bitbybit/models.py
+++ vscodemysite/bitbybit/models.py
@@ -4,3 +4,6 @@ class Task(models.Model):
     task_title = models.CharField(max_length=70)
     task_desc = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.task_title
--- vscodemysite/bitbybit/views.py
+++ vscodemysite/bitbybit/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from bitbybit.models import Task
 
 def tasks(request):
-    return render(request, "tasks.html")
+    all_tasks = Task.objects.all()
+    if request.method == "POST":
+        title = request.POST["title"]
+        desc = request.POST["desc"]
+        print(title, desc)
+        ins = Task(task_title=title, task_desc=desc)
+        ins.save()
+        context = {"tasks": all_tasks}
+    context = {"tasks": all_tasks}
+    return render(request, "tasks.html", context)
